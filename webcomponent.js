751f33dfa8e1dfeb53160484a64a9bf703898ec0
renatobnd <58137329+renatobnd@users.noreply.github.com>
2022-06-20 06:52:25 -0300

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,18 +1,19 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-      <h1>
-      Hello World Custom Widgets!
-      </h1>
+     
     `;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
 
 
 		constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			    super(); 
+			    this._shadowRoot = this.attachShadow({mode: "open"});
+			    this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			    this._tagContainer;
+			    this._tagType = "h1";
+			    this._tagText = "Hello World";
 		}
 
 
@@ -53,9 +54,19 @@
         */
 
         redraw(){
-        }
-    
-    
-    });
+		    if (this._tagText != null){
+			if (this._tagContainer){
+			    this._tagContainer.parentNode.removeChild(this._tagContainer);
+			}
+
+			var shadow = window.getSelection(this._shadowRoot);
+			this._tagContainer = document.createElement(this._tagType);
+			var theText = document.createTextNode(this._tagText);    
+			this._tagContainer.appendChild(theText); 
+			this._shadowRoot.appendChild(this._tagContainer);
+		    }
+	
+     
+	});
         
 })();
