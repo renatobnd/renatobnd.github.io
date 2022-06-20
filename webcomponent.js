930e2263812f543f00ac9ff40f4591c851bca52a
renatobnd <58137329+renatobnd@users.noreply.github.com>
2022-06-20 12:41:41 -0300

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -7,14 +7,21 @@
 
 
 		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-            this._tagContainer;
-            this._tagType = "h1";
-            this._tagText = "Hello World";
+		    super();
+		    this._shadowRoot = this.attachShadow({mode: "open"});
+		    this._shadowRoot.appendChild(template.content.cloneNode(true));
+		    this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		}
 
+		_submit(e) {
+		    e.preventDefault();
+		    this.dispatchEvent(new CustomEvent("propertiesChanged", {
+			    detail: {
+				properties: {
+				    widgetText: this.widgetText
+				}
+			    }
+		    }));
 		}
 
         //Fired when the widget is added to the html DOM of the page
